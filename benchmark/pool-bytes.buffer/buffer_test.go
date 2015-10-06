9ebfe7c4e2b623ce0b77fba2e4fcf9b1e80b014b
Daisuke Maki <lestrrat+github@gmail.com>
2015-10-06 19:24:19 +0900

    Add benchmark

---
+++ benchmark/pool-bytes.buffer/buffer_test.go
@@ -0,0 +1,39 @@
+package bufferbench
+
+import (
+	"bytes"
+	"sync"
+	"testing"
+)
+
+var chars []byte
+func init() {
+	s := "acdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	chars = make([]byte, len(s))
+	for i, c := range s {
+		chars[i] = byte(c)
+	}
+}
+
+func BenchmarkWriteToBuffer(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		b := bytes.Buffer{}
+		for j := 0; j < 100; j++ {
+			b.Write(chars)
+		}
+	}
+}
+
+var pool = sync.Pool{}
+func BenchmarkWriteToBufferWithPool(b *testing.B) {
+	pool.New = func() interface{} {
+		return &bytes.Buffer{}
+	}
+	for i := 0; i < b.N; i++ {
+		b := pool.Get().(*bytes.Buffer)
+		for j := 0; j < 100; j++ {
+			b.Write(chars)
+		}
+		pool.Put(b)
+	}
+}
