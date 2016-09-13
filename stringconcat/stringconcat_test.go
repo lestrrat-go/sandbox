9aaadc9329e869537c7b87cf83cc60348f310eb8
Daisuke Maki <lestrrat+github@gmail.com>
2016-09-13 12:19:51 +0900

    use the same variables, just to make sure

--- stringconcat/stringconcat_test.go
+++ stringconcat/stringconcat_test.go
@@ -9,14 +9,15 @@ import (
 	"testing"
 )
 
+var s1 = "abcdefghijklmnopqrstuvwxyz"
+var b1 = []byte{'a', 'b', 'c'}
+var i1 = 100
+
 func BenchmarkPlusConcat(b *testing.B) {
 	var s string
 	for i := 0; i < b.N; i++ {
 		s = ""
-		s = s +
-			"abcdefghijklmnopqrstuvwxyz" +
-			string([]byte{'a', 'b', 'c'}) +
-			strconv.Itoa(100)
+		s = s + s1 + string(b1) + strconv.Itoa(i1)
 		io.WriteString(ioutil.Discard, s)
 	}
 }
@@ -24,11 +25,7 @@ func BenchmarkPlusConcat(b *testing.B) {
 func BenchmarkFmtSprintf(b *testing.B) {
 	var s string
 	for i := 0; i < b.N; i++ {
-		s = fmt.Sprintf("%s%s%d",
-			"abcdefghijklmnopqrstuvwxyz",
-			[]byte{'a', 'b', 'c'},
-			100,
-		)
+		s = fmt.Sprintf("%s%s%d", s1, b1, i1)
 		io.WriteString(ioutil.Discard, s)
 	}
 }
@@ -37,9 +34,9 @@ func BenchmarkBytesBuffer(b *testing.B) {
 	var buf bytes.Buffer
 	for i := 0; i < b.N; i++ {
 		buf.Reset()
-		buf.WriteString("abcdefghijklmnopqrstuvwxyz")
-		buf.Write([]byte{'a', 'b', 'c'})
-		buf.WriteString(strconv.Itoa(100))
+		buf.WriteString(s1)
+		buf.Write(b1)
+		buf.WriteString(strconv.Itoa(i1))
 		io.WriteString(ioutil.Discard, buf.String())
 	}
 }
@@ -47,9 +44,9 @@ func BenchmarkBytesBuffer(b *testing.B) {
 func BenchmarkAppendBytes(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		buf := make([]byte, 0, 64)
-		buf = append(buf, "abcdefghijklmnopqrstuvwxyz"...)
-		buf = append(buf, []byte{'a', 'b', 'c'}...)
-		buf = strconv.AppendInt(buf, 100, 10)
+		buf = append(buf, s1...)
+		buf = append(buf, b1...)
+		buf = strconv.AppendInt(buf, int64(i1), 10)
 		io.WriteString(ioutil.Discard, string(buf))
 	}
 }
@@ -59,9 +56,9 @@ func BenchmarkAppendBytes(b *testing.B) {
 func BenchmarkAppendBytesNoCap(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		var buf []byte
-		buf = append(buf, "abcdefghijklmnopqrstuvwxyz"...)
-		buf = append(buf, []byte{'a', 'b', 'c'}...)
-		buf = strconv.AppendInt(buf, 100, 10)
+		buf = append(buf, s1...)
+		buf = append(buf, b1...)
+		buf = strconv.AppendInt(buf, int64(i1), 10)
 		io.WriteString(ioutil.Discard, string(buf))
 	}
 }
